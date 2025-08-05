f1bd77abca26bd9cf26fd1f6eee549ea9e9f8d91
rifqinailal <rifqinailal@gmail.com>
2025-08-05 11:10:53 +0700

    update

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,6 @@ import helmet from 'helmet';
 import dotenv from 'dotenv';
 import path from 'path';
 
-// Muat environment variables dari file .env
-
-// Helper untuk mendapatkan __dirname di ES Module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 // Impor router utama dan helper
 import mainRouter from './routes/index.js';
 import { sendError } from './utils/apiResponse.js'; // Asumsi lokasi helper
@@ -26,8 +20,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use('/uploads', express.static('uploads'));
-app.use('/uploads', express.static(path.join(__dirname, '../public/uploads')));
+
 
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
