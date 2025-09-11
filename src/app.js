beca9afb9b694a6aa023d75be74d43c1e901aaa7
rifqinailal <rifqinailal@gmail.com>
2025-09-11 10:06:07 +0700

    Menambahkan header 'Access-Control-Allow-Origin' ke setiap file

--- src/app.js
+++ src/app.js
@@ -22,7 +22,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads"), {
+  setHeaders: (res, path, stat) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+  }
+}));
 
 /*
 ============================================================
